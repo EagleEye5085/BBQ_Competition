53ae164cec6c4cd8bd90412d3fb828f203c53804
Thomas Turner <thomasturner482@yahoo.com>
2022-07-07 11:17:19 -0500

    basic parent and child

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/submissions', to: 'submissions#index'
+  get '/teams', to: 'teams#index'
 end
---
+++ db/migrate/20220707153100_create_submissions.rb
@@ -0,0 +1,14 @@
+class CreateSubmissions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :submissions do |t|
+      t.string :title
+      t.string :meat
+      t.string :rub
+      t.string :sauce
+      t.integer :cook_time
+      t.float :score
+
+      t.timestamps
+    end
+  end
+end
